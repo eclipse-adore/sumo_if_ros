c1a94914d18ea85a39b1b89b9ffc982d7d4d8c37
Marko Mizdrak <marko.mizdrak@dlr.de>
2025-11-12 11:09:45 +0100

    cmake does not depend on external functions, code formatted with parent .clang-format

--- include/adore_sumo_traffic.hpp
+++ include/adore_sumo_traffic.hpp
@@ -14,95 +14,98 @@
 #pragma once
 
 #define _USE_MATH_DEFINES
-#include <math.h>
 #include <libsumo/libsumo.h>
+#include <math.h>
 
-#include <adore_ros2_msgs/msg/traffic_participant.hpp>
-#include <adore_ros2_msgs/msg/traffic_participant_set.hpp>
 #include <chrono>
 #include <iostream>
-#include <rclcpp/rclcpp.hpp>
-#include <rclcpp/time.hpp>
-#include <std_msgs/msg/float64.hpp>
 #include <thread>
 #include <unordered_map>
 #include <unordered_set>
 
+#include <adore_ros2_msgs/msg/traffic_participant.hpp>
+#include <adore_ros2_msgs/msg/traffic_participant_set.hpp>
+
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/time.hpp>
+#include <std_msgs/msg/float64.hpp>
+
 using namespace std::chrono_literals;
 
-namespace adore {
-namespace sumo_if_ros {
-struct Timer {
- public:
+namespace adore
+{
+namespace sumo_if_ros
+{
+struct Timer
+{
+public:
+
   double tUTC_;
   Timer();
-  void receive(const std_msgs::msg::Float64& msg);  // callback for ros time
+  void receive( const std_msgs::msg::Float64& msg ); // callback for ros time
 };
 
-struct ROSVehicleSet {
- public:
-  std::unordered_map<int, adore_ros2_msgs::msg::TrafficParticipant>
-      data;  ///<- a mapping from vehicle
-              ///< id to
-              ///< latest message
-  void receive(const adore_ros2_msgs::msg::TrafficParticipant&
-                   msg);  // callback for receiving ros vehicle data
+struct ROSVehicleSet
+{
+public:
+
+  std::unordered_map<int, adore_ros2_msgs::msg::TrafficParticipant> data; ///<- a mapping from vehicle
+                                                                          ///< id to
+                                                                          ///< latest message
+  void receive( const adore_ros2_msgs::msg::TrafficParticipant& msg );    // callback for receiving ros vehicle data
 };
 
-class SUMOTrafficToROS : public rclcpp::Node {
- public:
+class SUMOTrafficToROS : public rclcpp::Node
+{
+public:
+
   SUMOTrafficToROS();
 
- protected:
-  ROSVehicleSet ros_vehicle_set;  // ros vehicle data
+protected:
 
-  rclcpp::Publisher<adore_ros2_msgs::msg::TrafficParticipantSet>::SharedPtr
-      publisher;  // publisher for sumo traffic data
-  rclcpp::Subscription<adore_ros2_msgs::msg::TrafficParticipant>::SharedPtr
-      subscriber;                     // subscription of dynamic vehicle state
-  std::string sumo_rosveh_prefix;     // prefix for sumo ros vehicles
-  rclcpp::TimerBase::SharedPtr timer;  // main timer
+  ROSVehicleSet ros_vehicle_set; // ros vehicle data
 
-  std::unordered_map<std::string, int>
-      sumo_veh_id_to_int;         // mapping for id of sumo vehicles
-  int last_assigned_int_id;  // last used id
-  std::map<int, std::string>
-      replacement_ids;  // additional feature to replace certain ids
+  rclcpp::Publisher<adore_ros2_msgs::msg::TrafficParticipantSet>::SharedPtr publisher;          // publisher for sumo traffic data
+  rclcpp::Subscription<adore_ros2_msgs::msg::TrafficParticipant>::SharedPtr subscriber;         // subscription of dynamic vehicle state
+  std::string                                                               sumo_rosveh_prefix; // prefix for sumo ros vehicles
+  rclcpp::TimerBase::SharedPtr                                              timer;              // main timer
 
-  std::vector<std::string> veh_id_list;  // list of sumo vehicles
-  std::vector<std::string>
-      sumo_to_ros_ignore_list;  // additional feature to ignore certain ids
-  std::vector<int>
-      ros_to_sumo_ignore_list;  // additional feature to ignore certain ids
+  std::unordered_map<std::string, int> sumo_veh_id_to_int;   // mapping for id of sumo vehicles
+  int                                  last_assigned_int_id; // last used id
+  std::map<int, std::string>           replacement_ids;      // additional feature to replace certain ids
 
-  rcl_time_point_value_t ros_time;  // current ros time
-  rcl_time_point_value_t tROS0;     // ros time at startup
-  double tSUMO0;                    // sumo time at startup
-  double tSUMO;                     // current sumo time
-  double step_length;               // step length in s
+  std::vector<std::string> veh_id_list;             // list of sumo vehicles
+  std::vector<std::string> sumo_to_ros_ignore_list; // additional feature to ignore certain ids
+  std::vector<int>         ros_to_sumo_ignore_list; // additional feature to ignore certain ids
 
- public:
- protected:
-  int get_new_int_id();                              // get new unique integer id
-  void remove_vehicle(std::string& id);            // remove vehicle in sumo
-  void add_vehicle(std::string& id);               // add vehicle in sumo
-  void set_max_speed(std::string& id, double val);  // set max speed in sumo
-  void set_speed(std::string& id, double val);     // set speed in sumo
-  void move_to_xy(std::string& id, std::string z, int a, double x, double y,
-                double heading, int b);  // move vehicle in sumo
+  rcl_time_point_value_t ros_time;    // current ros time
+  rcl_time_point_value_t tROS0;       // ros time at startup
+  double                 tSUMO0;      // sumo time at startup
+  double                 tSUMO;       // current sumo time
+  double                 step_length; // step length in s
 
- public:
-  void run_callback();  // run function triggered by timer
-  rcl_time_point_value_t sumo_to_ros_time(
-      double sumo_time);  // conversion of sumo time to ros time
-  double ros_to_sumo_time(
-      rcl_time_point_value_t ros_time);  // conversion of ros time to sumo time
-  bool new_step();                        // perform new step in sumo
-  void transfer_data_sumo_to_ros();          // transfer data from sumo to ros
-  void transfer_data_ros_to_sumo();          // transfer data from ros to sumo
+public:
+
+protected:
+
+  int  get_new_int_id();                                                                               // get new unique integer id
+  void remove_vehicle( std::string& id );                                                              // remove vehicle in sumo
+  void add_vehicle( std::string& id );                                                                 // add vehicle in sumo
+  void set_max_speed( std::string& id, double val );                                                   // set max speed in sumo
+  void set_speed( std::string& id, double val );                                                       // set speed in sumo
+  void move_to_xy( std::string& id, std::string z, int a, double x, double y, double heading, int b ); // move vehicle in sumo
+
+public:
+
+  void                   run_callback();                                      // run function triggered by timer
+  rcl_time_point_value_t sumo_to_ros_time( double sumo_time );                // conversion of sumo time to ros time
+  double                 ros_to_sumo_time( rcl_time_point_value_t ros_time ); // conversion of ros time to sumo time
+  bool                   new_step();                                          // perform new step in sumo
+  void                   transfer_data_sumo_to_ros();                         // transfer data from sumo to ros
+  void                   transfer_data_ros_to_sumo();                         // transfer data from ros to sumo
 
   void init_sumo();  // initialize sumo
-  void close_sumo();  // close sumo
+  void close_sumo(); // close sumo
 };
-}  // namespace sumo_if_ros
-}  // namespace adore
+} // namespace sumo_if_ros
+} // namespace adore
